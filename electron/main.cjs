dff52761b9ec9768b6739f4e9eec0abb51f706e8
Lucas <lucasblancoalv14@gmail.com>
2026-05-14 12:16:25 +0200

    Proyecto Instalador DID

---
+++ electron/main.cjs
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 720,
+    minWidth: 480,
+    minHeight: 480,
+    title: 'Task App',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools({ mode: 'detach' });
+  } else {
+    win.loadFile(path.join(__dirname, '..', 'dist', 'index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
